1ee808abee1c038938b87e8b507f568d8e8dbc9d
Andrea Trasviña <alucerotrasvina@gmail.com>
2024-02-29 12:24:47 -0700

    CSS y Javascript con vite

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,6 +7,9 @@
 {{--    <title>Aprendible - @yield('title')</title>--}}
     <title>Aprendible - {{ $title ?? '' }}</title>
     <meta name="description" content=" {{ $metaDescription ?? 'Default meta description' }}" />
+{{--    <link rel="stylesheet" href="/css/app.css">--}}
+{{--    <script src="/js/app.js"></script>--}}
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
