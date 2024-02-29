c633f3dd2399dd850b905ce9dad9936e0cde6c8a
Andrea Trasviña <alucerotrasvina@gmail.com>
2024-02-29 12:51:48 -0700

    Ejemplo de css con sass

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,9 +7,9 @@
 {{--    <title>Aprendible - @yield('title')</title>--}}
     <title>Aprendible - {{ $title ?? '' }}</title>
     <meta name="description" content=" {{ $metaDescription ?? 'Default meta description' }}" />
-{{--    <link rel="stylesheet" href="/css/app.css">--}}
+{{--    <link rel="stylesheet" href="/css/app.scss">--}}
 {{--    <script src="/js/app.js"></script>--}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
 
 </head>
 
