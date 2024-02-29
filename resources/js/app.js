172bf7339ed3ad53dca804565d59cf97e739b450
Andrea Trasviña <alucerotrasvina@gmail.com>
2024-02-29 13:29:15 -0700

    Tailwind CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,5 @@ import './bootstrap'
 
 import '../css/app.scss'
 
-import * as bootstrap from 'bootstrap'
-
-// Ejemplo de uso de un componente de Bootstrap (por ejemplo, un modal)
-// porque no funciona la libreria sin usar un componente XD
-const myModal = new bootstrap.Modal(document.getElementById('myModal'));
 
 console.log('JS LOADED')
